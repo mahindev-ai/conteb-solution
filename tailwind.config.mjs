125341969a6cd813f95c52c56ceba60eff055b5c
mahindev-ai <mahindev.ai@gmail.com>
2024-04-16 10:05:49 -0500

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Inter Variable", ...defaultTheme.fontFamily.sans],
+				title:['Sora', 'sans-serif']
+			},
+			colors: {
+				primary: "var(--color-primary)",
+				secondary: "var(--color-secondary)",
+			},
+			textColor: {
+				default: "var(--color-text)",
+				offset: "var(--color-text-offset)",
+			},
+			backgroundColor: {
+				default: "var(--color-background)",
+				offset: "var(--color-background-offset)",
+			},
+			borderColor: {
+				default: "var(--color-border)",
+			},
+		},
+	},
+	corePlugins:{
+		fontSize: false,
+	},
+	plugins: [require("tailwindcss-fluid-type")],
+}
